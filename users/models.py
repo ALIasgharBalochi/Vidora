97a7ae6b21bcd50e2199c78061c45114da7075c5
ali_balochi <alibalochi1910@gmail.com>
2026-09-15 16:46:04 +0330

    create models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Plans(models.Model):
+    plane_choises = [("br", "Bronze"), ("sl", "Silver"), ("gl", "Gold")]
+    plane_name = models.CharField(choices=plane_choises)
+    price = models.FloatField()
+
+
+class CustomUser(AbstractUser):
+    plan = models.ForeignKey(Plans, on_delete=models.PROTECT, related_name="users")
+
+
+class Transactin(models.Model):
+    price = models.FloatField()
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="transactions"
+    )
+    plane = models.ForeignKey(
+        Plans, on_delete=models.SET_NULL, related_name="transactions"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Video(models.Model):
+    url = models.CharField()
+    like_count = models.PositiveIntegerField(default=0)
+    rating = models.FloatField(default=0.0)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, models.CASCADE, related_name="comments")
+    video = models.ForeignKey(Video, models.CASCADE, related_name="comments")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class WatchedVideo(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="watched_videos"
+    )
+    video = models.ForeignKey(Video, related_name="watched_videos")
+    watched_minuts = models.IntegerField()
